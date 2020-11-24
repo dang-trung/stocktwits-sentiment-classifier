72eb09fb6e98505b7a9e6b0ecbc4129a07873424
Trung Dang <dangtrung96@gmail.com>
2020-11-24 01:49:43 +0100

    Re-organize project structure.

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+import pathlib
+from setuptools import setup, find_packages
+
+wd = pathlib.Path(__file__).parent.resolve()
+long_description = (wd / 'README.md').read_text(encoding='utf-8')
+with open(wd / 'requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='sent-classifier',
+    version='0.0.0',
+    description='Classify sentiments of StockTwits crypto-related messages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dang-trung/stocktwits-sentiment-classifier',
+    author='Trung Dang',
+    author_email='dangtrung96@gmail.com',
+    package_dir={'': 'src'},
+    packages=find_packages(include=['src', 'src.*']),
+    package_data={'': ['[file-to-be-include-in-package]']},
+    python_requires='>=3.5, <4',
+    install_requires=requirements,
+)
